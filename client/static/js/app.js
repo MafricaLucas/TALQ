eed4346da4ec38b19f35c8f15a30f62c5b1d87a7
MafricaLucas <32304399+MafricaLucas@users.noreply.github.com>
2018-09-11 09:15:43 +0200

    Add files via upload

---
+++ client/static/js/app.js
@@ -0,0 +1,55 @@
+/**
+* Constants
+**/
+
+
+//var API_URL = 'https://www.waow.eu/api/';
+//var UPLOADS_URL = 'https://www.waow.eu/';
+
+var app = angular.module("app", ['ngRoute']);
+
+app.config(function($routeProvider) {
+
+  /** Routing */
+
+  $routeProvider.when('/', {
+    templateUrl: 'static/tpl/home.html',
+    controller: 'home_controller'
+  }).when('/conv', {
+    templateUrl: 'static/tpl/conv.html',
+    controller: 'home_controller'
+  }).otherwise({
+    templateUrl: 'static/tpl/error.html',
+    controller: 'home_controller',
+    doNotTrack: true
+  });
+
+
+});
+
+app.run(function($rootScope, $http, $location, $route, $timeout, $interval, $window) {
+
+  $rootScope.variable = "test";
+  $rootScope.bool = true;
+  console.log('test');
+  $rootScope.testHome = function() {
+    $rootScope.bool = false;
+  }
+
+
+
+});
+
+
+app.directive('wHref', function() {
+  return {
+    restrict: 'A',
+    scope: {
+      url: '@wHref'
+    }, link: function(scope, element, attrs) {
+      scope.$watch('url', function(n,o) {
+        element[0].href = ('#!' + n);
+      });
+    }
+  }
+});
---
+++ client/static/js/app/home_controller.js
@@ -0,0 +1,8 @@
+app.controller("home_controller", function($scope, $rootScope, $location, $http, $timeout, $routeParams, $window) {
+
+    console.log('reussi');
+
+  $scope.bonjour = function(){
+    console.log('reussi');
+  }
+});
