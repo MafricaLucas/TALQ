c51822c258a36a7619e1694f5f19782f0f9c92a9
MafricaLucas <32304399+MafricaLucas@users.noreply.github.com>
2018-09-14 17:46:25 +0200

    Add files via upload

--- client/static/js/app/home_controller.js
+++ client/static/js/app/home_controller.js
@@ -1,8 +1,19 @@
 app.controller("home_controller", function($scope, $rootScope, $location, $http, $timeout, $routeParams, $window) {
 
-
+  $scope.signUpStep = 0;
+  $scope.user = {
+    first_name: null,
+    last_name: null,
+    mail: null,
+    language: null,
+    gender: null,
+    birthday: null,
+    day: null,
+    month: null,
+    year: null,
+  }
   $rootScope.header=false;
-  $scope.bonjour = function(){
-    console.log('reussi');
+  $scope.connexion = function(){
+    console.log($scope.user);
   }
 });
